65f4c00c3afc015d99e5bef4996a436f6e89a7b8
Stephen Margheim <stephen.margheim@gmail.com>
2023-08-22 20:23:47 +0200

    Fix the summarizer
    to handle summaries in the middle of paragraphs

--- plugins/builders/summarizer.rb
+++ plugins/builders/summarizer.rb
@@ -1,10 +1,25 @@
 class Builders::Summarizer < SiteBuilder
   def build
     define_resource_method :summary_extension_output do
-      return "<p>#{data[:summary]}</p>" if data.key?(:summary)
-      return content.split(site.config[:summary_separator], 2).first if site.config.key?(:summary_separator) && content.match?(site.config[:summary_separator])
-
-      content.to_s.strip.lines.first.to_s.strip.html_safe
+      summary = if data.key?(:summary)
+        data[:summary]
+      elsif site.config.key?(:summary_separator) && content.match?(site.config[:summary_separator])
+        content.split(site.config[:summary_separator], 2).first
+      else
+        content.to_s.strip.lines.first.to_s.strip.html_safe
+      end
+      
+      summary.strip!
+      
+      if !summary.start_with?('<p>') && !summary.end_with?('</p>')
+        "<p>#{summary}</p>"
+      elsif summary.start_with?('<p>') && !summary.end_with?('</p>')
+        "#{summary}</p>"
+      elsif !summary.start_with?('<p>') && summary.end_with?('</p>')
+        "<p>#{summary}"
+      elsif summary.start_with?('<p>') && summary.end_with?('</p>')
+        summary
+      end
     end
   end
 end
