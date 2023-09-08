1c34652a891153b1fb336a2c0341dd167b21496f
Stephen Margheim <stephen.margheim@gmail.com>
2023-09-08 08:41:14 +0200

    Debug the katex plugin on Cloudflare Pages

--- plugins/builders/katex.rb
+++ plugins/builders/katex.rb
@@ -8,7 +8,11 @@ class Builders::Katex < SiteBuilder
     inspect_html do |document|
       document.query_selector_all(SELECTOR).each do |element|
         result, _stderr_str, _status = Open3.capture3("npx katex", stdin_data: element.text)
-        element.replace(result.encode('UTF-8').strip)
+p '*' * 100
+p result.encoding
+p result.bytes
+p result.inspect
+        element.replace(result.encode('UTF-8', 'US-ASCII').strip)
       end
     end
   end
