c41b8a3ae5f1b3c8901e819e575af9b183f1b2b8
Stephen Margheim <stephen.margheim@gmail.com>
2023-09-08 09:27:56 +0200

    Finally fix the katex plugin on Cloudflare Pages

--- plugins/builders/katex.rb
+++ plugins/builders/katex.rb
@@ -8,12 +8,6 @@ class Builders::Katex < SiteBuilder
     inspect_html do |document|
       document.query_selector_all(SELECTOR).each do |element|
         result, _stderr_str, _status = Open3.capture3("npx katex", stdin_data: element.text)
-p '*' * 100
-p ['locale', Encoding.locale_charmap]
-p ['filesystem', Encoding.find('filesystem')]
-p ['external', Encoding.default_external]
-p ['internal', Encoding.default_internal]
-p ['result', result.encoding]
         element.replace(result.force_encoding('UTF-8').strip)
       end
     end
