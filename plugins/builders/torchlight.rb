d11686be9b54320ea9d978c2d006a8aa9433620f
Stephen Margheim <stephen.margheim@gmail.com>
2025-12-18 16:22:56 +0100

    Try disabling 1Password on code samples

--- plugins/builders/torchlight.rb
+++ plugins/builders/torchlight.rb
@@ -4,6 +4,18 @@ class Builders::Torchlight < SiteBuilder
       # next unless Bridgetown.env.production?
 
       system "yarn torchlight"
+      add_1p_ignore_to_pre_tags
+    end
+  end
+
+  def add_1p_ignore_to_pre_tags
+    Dir.glob("output/**/*.html").each do |file|
+      content = File.read(file)
+      # Add data-1p-ignore to pre tags that don't already have it
+      modified = content.gsub(/<pre(?![^>]*data-1p-ignore)/) do |match|
+        "#{match} data-1p-ignore"
+      end
+      File.write(file, modified) if content != modified
     end
   end
 end
